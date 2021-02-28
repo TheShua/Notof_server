e8f5044476a8e4b6c67c46562e448c889a25bb13
Luxia <nelly.borjon@hotmail.fr>
2021-02-28 19:26:04 +0100

    login/logout with passport

--- app.js
+++ app.js
@@ -6,20 +6,26 @@ const path = require('path');
 const cors = require('cors')
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const passport = require('passport');
+require('./config/passport/localStrategy')
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const loginRouter = require('./routes/auth');
 
 const app = express();
 
 app.use(cors({origin:process.env.FRONTEND_URL, credentials:true}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', loginRouter);
 
 module.exports = app;
