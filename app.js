a979b0880a7cc1cd9ff0174443c19598e656e53e
Shua <g.deglisezinski@gmail.com>
2021-03-23 22:12:40 +0100

    Mon bordel

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
-require("./config/mongo-connection");
+require('dotenv').config();
+require('./config/mongo-connection');
 
 const express = require('express');
 const path = require('path');
